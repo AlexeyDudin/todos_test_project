02639f675a26aa3ffa13813f5b1d46d8964f1643
Alexey Dudin <dudin.lesha@gmail.com>
2023-04-19 06:05:57 +0300

    AddTodoEntity

---
+++ src/Entity/Todo.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TodoRepository::class)
+ */
+class Todo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=1000)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $executed;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function isExecuted(): ?bool
+    {
+        return $this->executed;
+    }
+
+    public function setExecuted(bool $executed): self
+    {
+        $this->executed = $executed;
+
+        return $this;
+    }
+}
